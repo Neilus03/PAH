345f48d8418a196a0e901e9a1914a317ae724ad3
Neilus03 <neildlf@gmail.com>
2024-11-30 11:47:58 +0100

    Add support for multiple backbones in HyperCMTL model and remove ConvBackbone class

--- hypernetwork.py
+++ hypernetwork.py
@@ -12,6 +12,14 @@ from torchmeta.modules import MetaSequential, MetaLinear
 from metamodules import FCBlock, BatchLinear, HyperNetwork, get_subdict
 from torchmeta.modules import MetaModule
 
+from backbones import ResNet50, MobileNetV2, EfficientNetB0
+
+backbone_dict = {
+    'resnet50': ResNet50,
+    'mobilenetv2': MobileNetV2,
+    'efficientnetb0': EfficientNetB0
+}
+
 class HyperCMTL(nn.Module):
     """
     Hypernetwork-based Conditional Multi-Task Learning (HyperCMTL) model.
@@ -36,7 +44,7 @@ class HyperCMTL(nn.Module):
     """
     def __init__(self,
                  num_instances=1,
-                 #backbone_layers=[32, 64, 128, 256, 512],  # Backbone architecture
+                 backbone='resnet50',  # Backbone architecture
                  task_head_projection_size=64,             # Task head hidden layer size
                  task_head_num_classes=2,                  # Task head output size
                  hyper_hidden_features=256,                # Hypernetwork hidden layer size
@@ -48,7 +56,7 @@ class HyperCMTL(nn.Module):
         super().__init__()
 
         self.num_instances = num_instances
-        #self.backbone_layers = backbone_layers
+        self.backbone = backbone
         self.task_head_projection_size = task_head_projection_size
         self.task_head_num_classes = task_head_num_classes
         self.hyper_hidden_features = hyper_hidden_features
@@ -62,8 +70,10 @@ class HyperCMTL(nn.Module):
                                      input_size=(channels, img_size[0], img_size[1]),
                                      device=device)
         '''
-        self.backbone = ConvBackbone(pretrained=True, device=device)
-
+        if backbone in backbone_dict:
+            self.backbone = backbone_dict[backbone](device=device, pretrained=True)
+        else: 
+            raise ValueError(f"Backbone {backbone} is not supported.")
         # Task head
         self.task_head = TaskHead(input_size=self.backbone.num_features,
                                   projection_size=task_head_projection_size,
@@ -119,96 +129,6 @@ class HyperCMTL(nn.Module):
         optimizer_list.extend(self.task_head.get_optimizer_list())
         print("optimizer_list", optimizer_list)
         return optimizer_list
-
-'''
-class ConvBackbone(nn.Module):
-    def __init__(self,
-                 layers=[32, 64, 128, 256, 512], # list of conv layer num_kernels
-                 input_size=(1,32,32), # for grayscale images
-                 device="cuda",
-                ):
-        super().__init__()
-
-        in_channels, in_h, in_w = input_size
-
-        # build the sequential stack of conv layers:
-        conv_layers = []
-        prev_layer_size = in_channels
-        for layer_size in layers:
-            conv_layers.append(nn.Conv2d(prev_layer_size, layer_size, (3,3), padding='same'))
-            conv_layers.append(nn.ReLU())
-            prev_layer_size = layer_size
-        self.conv_stack = torch.nn.Sequential(*conv_layers)
-
-        # global average pooling to reduce to single dimension:
-        self.pool = nn.AdaptiveAvgPool2d((1,1))
-
-        # number of output features:
-        self.num_features = prev_layer_size
-
-        self.relu = nn.ReLU()
-
-        self.device = device
-        self.to(device)
-
-    def forward(self, x):
-        # pass through conv layers:
-        x = self.conv_stack(x)
-
-        # downsample and reshape to vector:
-        x = self.pool(x)
-        x = x.view(-1, self.num_features)
-
-        # return feature vector:
-        return x
-    
-    def get_optimizer_list(self):
-        optimizer_list = [{'params': self.parameters(), 'lr': 1e-3}]
-        return optimizer_list
-'''
-
-class ConvBackbone(nn.Module):
-    def __init__(self, pretrained=True, device="cuda"):
-        super().__init__()
-
-        # Load pretrained ResNet-50
-        resnet = resnet50(pretrained=pretrained)
-        print("resnet:", resnet)
-        #Freeze the first few layers
-        # for name, param in resnet.named_parameters():
-        #     if 'layer3' not in name and 'layer4' not in name and 'fc' not in name:
-        #         param.requires_grad = False
-            
-            
-        
-        # Remove the fully connected layer and retain only the convolutional backbone
-        self.feature_extractor = nn.Sequential(
-            *(list(resnet.children())[:-2])  # Removes FC and avg pooling
-        )
-        
-        # Add adaptive average pooling to reduce feature maps to 1x1
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        
-        # Set the number of output features (512 for ResNet-50)
-        self.num_features = resnet.fc.in_features
-
-        self.device = device
-        self.to(device)
-
-    def forward(self, x):
-        # Pass through ResNet backbone
-        x = self.feature_extractor(x)
-        
-        # Global average pooling to get feature vector
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
-
-        return x
-    
-    def get_optimizer_list(self):
-        # Add a lower learning rate for the pretrained parameters
-        return [{'params': self.feature_extractor.parameters(), 'lr': 1e-4}]
-
 
 class TaskHead(MetaModule):
     def __init__(self, input_size: int, # number of features in the backbone's output
